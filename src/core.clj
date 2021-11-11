2e48be8ce1b1e8b8bdc4ae84515a593d4f89e122
Mike Stockdale <mikes@syterra.com>
2021-11-10 17:07:24 -0700

    index documents

--- src/core.clj
+++ src/core.clj
@@ -7,17 +7,31 @@
   (println file-path)
   (load-file file-path))
 
-(defn publish-file [f]
+(defn get-doc-data [f]
   (let
-    [doc-path (str "docs/" (clojure.string/replace (.getName f) "page" "html"))]
-    (->> f .getPath eval-file (spit doc-path))))
+    [doc-name (clojure.string/replace (.getName f) "page" "html")
+     doc-path (str "docs/" doc-name)
+     doc-data (->> f .getPath eval-file)]
+    (cons doc-name (cons doc-path doc-data))))
 
-(defn -main []
-  (->> "content/publish.css" slurp (spit "docs/publish.css"))
-  (eval-file "content/core.defs")
+(defn write-file [doc-data]
+  (spit (nth doc-data 1) (nth doc-data 2)))
+
+(defn make-doc-def [doc-data]
+  (str "(list \"" (nth doc-data 0) "\" \"" (nth doc-data 3) "\")"))
+
+(defn walk-files [action]
   (->> "content"
        clojure.java.io/file
        .listFiles
        (filter-files ".page")
-       (map publish-file)
+       (map get-doc-data)
+       (map action)
        doall))
+
+(defn -main []
+  (->> "content/publish.css" slurp (spit "docs/publish.css"))
+  (eval-file "content/core.defs")
+  ;(println (str "(def documents (list" (clojure.string/join " " (walk-files make-doc-def)) "))"))
+  (eval (read-string (str "(def documents (list" (clojure.string/join " " (walk-files make-doc-def)) "))")))
+  (walk-files write-file))
